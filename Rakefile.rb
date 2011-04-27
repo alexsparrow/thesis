5f49b4982390e73cd2ee994ae0755688af56b2b9
Alex Sparrow <alex.sparrow@cern.ch>
2011-04-27 20:36:47 +0200

    Starting the thesis. Pretty much just the scaffolding right now.

---
+++ Rakefile.rb
@@ -0,0 +1,19 @@
+require 'rake-latex'
+
+Rake.startfile(__FILE__)
+
+# LaTeX document in mydoc.tex.
+latex('thesis')
+
+latex('thesis') do
+  |task|
+  task.includes = [
+    "ch1-theory.tex",
+    "ch2-expt.tex",
+    "ch3-wpol.tex",
+    "ch4-susy.tex",
+    "ch5-interpretation.tex"
+  ]
+end
+
+Rake.endfile
