94159b26396915ccd9cdf166f66624f6b64a9e3f
Alex Sparrow <alex.sparrow@cern.ch>
2012-03-19 17:03:19 +0100

    A load of changes

--- tools/greps.sh
+++ tools/greps.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+echo "mSUGRA"
+echo "=================================="
 grep -i -r -n --include "*.tex" msugra *
+echo "=================================="
+
+echo "TODO"
+echo "=================================="
+grep -i -r -n --include "*.tex" todo *
+echo "=================================="
+
+echo "cutflow"
+echo "=================================="
+grep -i -r -n --include "*.tex" cutflow *
+echo "=================================="
+
+echo "SMS"
+echo "=================================="
+grep -i -r -n --include "*.tex" sms *
+echo "=================================="
